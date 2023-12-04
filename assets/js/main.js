5e04f4c16eba82e8025cd5eef000cdb784c6ea08
biaccshop <biaccshopinfo@gmail.com>
2023-12-04 07:00:37 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-function ctrlShiftKey(e, keyCode) {
-    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-document.onkeydown = (e) => {
-    if (
-        event.keyCode === 123 ||
-        ctrlShiftKey(e, 'I') ||
-        ctrlShiftKey(e, 'J') ||
-        ctrlShiftKey(e, 'C') ||
-        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-    )
-        return false;
-};
-
-
-
 // Scroll Reveal
 ScrollReveal({
   reset: false,
@@ -25,34 +6,9 @@ ScrollReveal({
   delay: 200
 });
 
-ScrollReveal().reveal('.hero-content', { origin: 'left', distance: '0px', delay: 200 });
-ScrollReveal().reveal('.hero-img', { origin: 'right', distance: '0px', delay: 200 });
-ScrollReveal().reveal('.whyus', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.features-img', { origin: 'right', distance: '0px', delay: 200, duration: 3500 });
-ScrollReveal().reveal('.features-content .feature-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.features-content .feature-1', { origin: 'right', distance: '50px', delay: 200 });
-ScrollReveal().reveal('.features-content .feature-2', { origin: 'right', distance: '50px', delay: 300 });
-ScrollReveal().reveal('.features-content .feature-3', { origin: 'right', distance: '50px', delay: 400 });
-ScrollReveal().reveal('.features-content .feature-4', { origin: 'right', distance: '50px', delay: 500 });
-ScrollReveal().reveal('.features-content .feature-5', { origin: 'right', distance: '50px', delay: 600 });
-ScrollReveal().reveal('.features-content .feature-6', { origin: 'right', distance: '50px', delay: 700 });
-ScrollReveal().reveal('.reasons .reasons-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.reasons-content .card-1', { origin: 'left', distance: '500px', delay: 400 });
-ScrollReveal().reveal('.reasons-content .card-2', { origin: 'left', distance: '500px', delay: 400 });
-ScrollReveal().reveal('.reasons-content .card-3', { origin: 'right', distance: '500px', delay: 400 });
-ScrollReveal().reveal('.reasons-content .card-4', { origin: 'right', distance: '500px', delay: 400 });
-ScrollReveal().reveal('.results .reasons-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.feedbacks .feedbacks-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.feedbacks .feedbacks-subtitle', { origin: 'right', distance: '0px', delay: 300, duration: 2500 });
-ScrollReveal().reveal('.feedbacks .slider', { origin: 'right', distance: '0px', delay: 200, duration: 2500 });
-ScrollReveal().reveal('.pricing .pricing-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.pricing .pricing-subtitle', { origin: 'right', distance: '0px', delay: 300, duration: 2500 });
-ScrollReveal().reveal('.pricing-content', { origin: 'right', distance: '0px', delay: 200, duration: 3500 });
-ScrollReveal().reveal('.payments .payments-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.payments .payments-methods', { origin: 'left', distance: '0px', delay: 400 });
-ScrollReveal().reveal('.faq .faq-title', { origin: 'right', distance: '0px', delay: 200, duration: 2000 });
-ScrollReveal().reveal('.faq-container', { origin: 'left', distance: '0px', delay: 400 });
-ScrollReveal().reveal('.contact', { origin: 'right', distance: '0px', delay: 300, duration: 2500 });
+ScrollReveal().reveal('.hero', { origin: 'left', distance: '0px', delay: 20 });
+
+
 
 // Navbar anchor + hiding # in URL
 $('a[href^="#"]').on('click', function (event) {
@@ -86,44 +42,4 @@ $(document).ready(function () {
       menuOpen = false;
     }
   });
-});
-
-
-
-// FAQ section
-const faqQuestions = document.querySelectorAll(".faq-container .question");
-
-faqQuestions.forEach((faqQuestion) => {
-  faqQuestion.addEventListener("click", () => {
-    faqQuestions.forEach((otherQuestion) => {
-      if (otherQuestion !== faqQuestion && otherQuestion.classList.contains("active")) {
-        const otherAnswer = otherQuestion.nextElementSibling;
-        otherAnswer.classList.remove("active");
-        otherQuestion.classList.remove("active");
-      }
-    });
-
-    const answer = faqQuestion.nextElementSibling;
-    answer.classList.toggle("active");
-    faqQuestion.classList.toggle("active");
-  });
-});
-
-
-
-// Announcement
-const announcementClose = document.querySelector(".announcement-close");
-const announcementSection = document.querySelector("#announcement");
-
-announcementClose.addEventListener("click", () => {
-  announcementSection.classList.add("hidden");
-  sessionStorage.setItem("announcementHidden", "true");
-});
-
-// Checking of refresh
-window.addEventListener("load", () => {
-  const isAnnouncementHidden = sessionStorage.getItem("announcementHidden");
-  if (isAnnouncementHidden === "true") {
-    announcementSection.classList.add("hidden");
-  }
 });
